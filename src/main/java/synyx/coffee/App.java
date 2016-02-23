efa9d4d7d6e809a4dd74b799e47d4948d2896c52
Robin de Silva Jayasinghe <robin@jayasinghe.de>
2016-02-23 07:43:33 +0100

    initial version (does not work, though)

---
+++ src/main/java/synyx/coffee/App.java
@@ -0,0 +1,43 @@
+package synyx.coffee;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static final String QUEUE_NAME = "hurz";
+
+    public static void main( String[] args ) throws IOException, TimeoutException
+    {
+        Optional<Connection > conn = Optional.empty();
+        Optional<Channel> channel = Optional.empty();
+
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("192.168.99.100");
+            conn = Optional.of(factory.newConnection());
+            channel = Optional.of(conn.get().createChannel());
+            channel.get().queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.get().basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+        finally {
+            if(channel.isPresent()) {
+                channel.get().close();
+            }
+            if(conn.isPresent()) {
+                conn.get().close();
+            }
+        }
+    }
+}
---
+++ src/main/java/synyx/coffee/ReceiverApp.java
@@ -0,0 +1,47 @@
+package synyx.coffee;
+
+import com.rabbitmq.client.*;
+import com.sun.tools.javac.main.Option;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by jayasinghe on 22/02/16.
+ */
+public class ReceiverApp {
+    private static final String QUEUE_NAME = "hurz";
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        Optional<Connection > conn = Optional.empty();
+        Optional<Channel> channel = Optional.empty();
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("192.168.99.100");
+
+            conn = Optional.of(factory.newConnection());
+            channel = Optional.of(conn.get().createChannel());
+
+            channel.get().queueDeclare(QUEUE_NAME, false, false, false, null);
+            System.out.println("waiting for messages.");
+
+            Consumer firstConsumer = new DefaultConsumer(channel.get()) {
+                public void handleDelivery(String s, Envelope envelope,
+                       AMQP.BasicProperties basicProperties, byte[] bytes) throws IOException {
+                    String receivedMsg = new String(bytes, "UTF-8");
+                    System.out.println("received message" + receivedMsg);
+                }
+            };
+            channel.get().basicConsume(QUEUE_NAME, true, firstConsumer);
+        }
+        finally {
+            if(channel.isPresent()) {
+                channel.get().close();
+            }
+            if(conn.isPresent()) {
+                conn.get().close();
+            }
+        }
+    }
+}
